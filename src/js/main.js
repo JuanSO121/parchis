11e80195afb0e1116789d0e23cb186ba3571537c
JuanSO121 <sanchezjuanjose0508@gmail.com>
2023-04-08 18:42:59 -0500

    Codigo de la profe

--- src/js/main.js
+++ src/js/main.js
@@ -1,96 +1,185 @@
-var scene = null, 
-    camera = null, 
-    renderer = null, 
-    myCanvas = null, 
-    controls = null; 
+/*
+	Phantom by HTML5 UP
+	html5up.net | @ajlkn
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+*/
 
-function start3dService() {
-    initScene();
-    createpieces(); 
-    animate(); 
+(function($) {
 
-    window.addEventListener( 'resize', onWindowResize, false );
-}
+	var	$window = $(window),
+		$body = $('body');
 
-function initScene() {
-    scene = new THREE.Scene();
-    scene.background=new THREE.Color(012075);
-    camera = new THREE.PerspectiveCamera( 75,  
-                                        window.innerWidth / window.innerHeight, 
-                                        0.1,  
-                                        1000 ); 
+	// Breakpoints.
+		breakpoints({
+			xlarge:   [ '1281px',  '1680px' ],
+			large:    [ '981px',   '1280px' ],
+			medium:   [ '737px',   '980px'  ],
+			small:    [ '481px',   '736px'  ],
+			xsmall:   [ '361px',   '480px'  ],
+			xxsmall:  [ null,      '360px'  ]
+		});
 
-    myCanvas = document.querySelector('.webgl'); 
-    renderer = new THREE.WebGLRenderer({canvas: myCanvas});
-    renderer.setSize( window.innerWidth, window.innerHeight );
-    document.body.appendChild( renderer.domElement );
+	// Play initial animations on page load.
+		$window.on('load', function() {
+			window.setTimeout(function() {
+				$body.removeClass('is-preload');
+			}, 100);
+		});
 
-    scene.add(camera);
-  
-    controls = new THREE.OrbitControls(camera, renderer.domElement);
-    camera.position.set(10, 15, 10);
-    controls.update();
+	// Touch?
+		if (browser.mobile)
+			$body.addClass('is-touch');
 
-    const size = 30;
-    const divisions = 30;
+	// Forms.
+		var $form = $('form');
 
-    const gridHelper = new THREE.GridHelper(size, 
-                                            divisions,
-                                            0x000, //color cruz
-                                            0xffffff);
-    scene.add( gridHelper );
+		// Auto-resizing textareas.
+			$form.find('textarea').each(function() {
 
-    const axesHelper = new THREE.AxesHelper(5);
-    scene.add( axesHelper );
+				var $this = $(this),
+					$wrapper = $('<div class="textarea-wrapper"></div>'),
+					$submits = $this.find('input[type="submit"]');
 
-    const directionalLight = new THREE.DirectionalLight( 0xffffff, 1);
-            scene.add( directionalLight );
+				$this
+					.wrap($wrapper)
+					.attr('rows', 1)
+					.css('overflow', 'hidden')
+					.css('resize', 'none')
+					.on('keydown', function(event) {
 
-    const textureLoader = new THREE.TextureLoader();
-    var texture = textureLoader.load('./src/img/mapa.png');
+						if (event.keyCode == 13
+						&&	event.ctrlKey) {
 
-    const geometry = new THREE.PlaneGeometry(30, 30);
-    const material = new THREE.MeshStandardMaterial( {color: 0xfffffff,
-                                                        map: texture,
-                                                        side: THREE.DoubleSide,
-                                                        specular:0,
-                                                        transparent: true,
-                                                        blending: THREE.NormalBlending
-                                                        } ); //THREE.MultiplyBlending, THREE.NormalBlending, THREE.AdditiveBlending, THREE.SubtractiveBlending
-    const plane = new THREE.Mesh(geometry, material);
-    scene.add(plane);
-    plane.rotation.x = Math.PI / 2;
-}
+							event.preventDefault();
+							event.stopPropagation();
 
-function getRndInteger(min, max) {
-    return Math.floor(Math.random() * (max - min + 1) ) + min;
-}
+							$(this).blur();
 
-function createpieces() {
-  camera.position.z = 5;
- }
+						}
 
-function animate() {
-    controls.update();
-	requestAnimationFrame( animate );
-	renderer.render( scene, camera );
-}
+					})
+					.on('blur focus', function() {
+						$this.val($.trim($this.val()));
+					})
+					.on('input blur focus --init', function() {
 
-function onWindowResize(){
+						$wrapper
+							.css('height', $this.height());
 
-    camera.aspect = window.innerWidth / window.innerHeight;
-    camera.updateProjectionMatrix();
+						$this
+							.css('height', 'auto')
+							.css('height', $this.prop('scrollHeight') + 'px');
 
-    renderer.setSize( window.innerWidth, window.innerHeight );
-}
+					})
+					.on('keyup', function(event) {
 
-function openForm() {
-    document.getElementById("myForm").style.display = "block";
-}
+						if (event.keyCode == 9)
+							$this
+								.select();
 
-function closeForm() {
-    document.getElementById("myForm").style.display = "none";
-}
+					})
+					.triggerHandler('--init');
 
+				// Fix.
+					if (browser.name == 'ie'
+					||	browser.mobile)
+						$this
+							.css('max-height', '10em')
+							.css('overflow-y', 'auto');
 
-console.log(THREE);
+			});
+
+	// Menu.
+		var $menu = $('#menu');
+
+		$menu.wrapInner('<div class="inner"></div>');
+
+		$menu._locked = false;
+
+		$menu._lock = function() {
+
+			if ($menu._locked)
+				return false;
+
+			$menu._locked = true;
+
+			window.setTimeout(function() {
+				$menu._locked = false;
+			}, 350);
+
+			return true;
+
+		};
+
+		$menu._show = function() {
+
+			if ($menu._lock())
+				$body.addClass('is-menu-visible');
+
+		};
+
+		$menu._hide = function() {
+
+			if ($menu._lock())
+				$body.removeClass('is-menu-visible');
+
+		};
+
+		$menu._toggle = function() {
+
+			if ($menu._lock())
+				$body.toggleClass('is-menu-visible');
+
+		};
+
+		$menu
+			.appendTo($body)
+			.on('click', function(event) {
+				event.stopPropagation();
+			})
+			.on('click', 'a', function(event) {
+
+				var href = $(this).attr('href');
+
+				event.preventDefault();
+				event.stopPropagation();
+
+				// Hide.
+					$menu._hide();
+
+				// Redirect.
+					if (href == '#menu')
+						return;
+
+					window.setTimeout(function() {
+						window.location.href = href;
+					}, 350);
+
+			})
+			.append('<a class="close" href="#menu">Close</a>');
+
+		$body
+			.on('click', 'a[href="#menu"]', function(event) {
+
+				event.stopPropagation();
+				event.preventDefault();
+
+				// Toggle.
+					$menu._toggle();
+
+			})
+			.on('click', function(event) {
+
+				// Hide.
+					$menu._hide();
+
+			})
+			.on('keydown', function(event) {
+
+				// Hide on escape.
+					if (event.keyCode == 27)
+						$menu._hide();
+
+			});
+
+})(jQuery);
